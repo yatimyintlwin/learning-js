9f2521bb58fbadbbe709a9826e22e377dc4a136e
yatimyintlwin <yatimyintlwin43@gmail.com>
2024-11-03 20:58:25 +0630

    [Add] Variables

---
+++ variables/exercise1.js
@@ -0,0 +1,14 @@
+var num1 = 5;
+var num2 = 10;
+var num3 = num1 + num2;
+console.log(num3);
+
+var $num = 4;
+console.log($num);
+
+var a;
+var b;
+a = 1;
+b = 2;
+var c = a + b;
+console.log(c);
---
+++ variables/exercise2.js
@@ -0,0 +1,14 @@
+{
+  let num4 = 3;
+  var num5 = 2;
+  let num6 = num4 + num5;
+  console.log(num6);
+}
+
+let a,
+  b,
+  c = 2;
+
+console.log(c);
+console.log(b);
+console.log(a);
